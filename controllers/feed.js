11a58515496fc9aceb536a2083ac0d506c62e031
Ercument <ercukalkann@gmail.con>
2024-05-20 16:46:58 +0300

    rest api preparation

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,18 @@
 exports.getPosts = (req, res, next) => {
+    res.status(200).json({
+        posts: [{
+            title: 'first post',
+            content: 'this is the first post!'
+        }]
+    });
+}
 
+exports.addPost = (req, res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    res.status(201).json({
+        message: 'Post created successfully',
+        post: { id: new Date().toISOString(), title: title, content: content }
+    });
 }
